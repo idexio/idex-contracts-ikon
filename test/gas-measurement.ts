33c260aedd0a15631cbf769bfa9dbf175fd0d6bb
Shamsundar Dileep <dileepfrog@gmail.com>
2023-03-16 14:16:28 -0700

    chore: gas measurement script

--- test/gas-measurement.ts
+++ test/gas-measurement.ts
@@ -1,3 +1,4 @@
+import type { ContractTransaction } from 'ethers';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 import { v1 as uuidv1 } from 'uuid';
 import { ethers, network } from 'hardhat';
@@ -6,6 +7,7 @@ import {
   baseAssetSymbol,
   buildIndexPrice,
   buildIndexPriceWithTimestamp,
+  buildIndexPriceWithValue,
   deployAndAssociateContracts,
   fundWallets,
   getLatestBlockTimestampInSeconds,
@@ -15,6 +17,10 @@ import {
   fundingPeriodLengthInMs,
   getExecuteTradeArguments,
   getOrderHash,
+  getTransferArguments,
+  getTransferHash,
+  getWithdrawArguments,
+  getWithdrawalHash,
   indexPriceToArgumentStruct,
   Order,
   OrderSide,
@@ -24,6 +30,7 @@ import {
 } from '../lib';
 import type { Exchange_v4, USDC } from '../typechain-types';
 import { increaseTo } from '@nomicfoundation/hardhat-network-helpers/dist/src/helpers/time';
+import { MarketStruct } from '../typechain-types/contracts/Exchange.sol/Exchange_v4';
 
 describe.skip('Gas measurement', function () {
   let buyOrder: Order;
@@ -33,6 +40,7 @@ describe.skip('Gas measurement', function () {
   let exitFundWallet: SignerWithAddress;
   let indexPriceServiceWallet: SignerWithAddress;
   let insuranceFundWallet: SignerWithAddress;
+  let marketStruct: MarketStruct;
   let ownerWallet: SignerWithAddress;
   let sellOrder: Order;
   let sellOrderSignature: string;
@@ -70,13 +78,32 @@ describe.skip('Gas measurement', function () {
     exchange = results.exchange;
     usdc = results.usdc;
 
+    marketStruct = {
+      exists: true,
+      isActive: false,
+      baseAssetSymbol,
+      chainlinkPriceFeedAddress: results.chainlinkAggregator.address,
+      indexPriceAtDeactivation: 0,
+      lastIndexPrice: 0,
+      lastIndexPriceTimestampInMs: 0,
+      overridableFields: {
+        initialMarginFraction: '5000000',
+        maintenanceMarginFraction: '3000000',
+        incrementalInitialMarginFraction: '1000000',
+        baselinePositionSize: '14000000000',
+        incrementalPositionSize: '2800000000',
+        maximumPositionSize: '282000000000',
+        minimumPositionSize: '10000000',
+      },
+    };
+
     await usdc.faucet(dispatcherWallet.address);
 
     await fundWallets(
       [trader1Wallet, trader2Wallet],
       exchange,
       results.usdc,
-      '100000.00000000',
+      '3000.00000000',
     );
 
     await exchange
@@ -148,7 +175,507 @@ describe.skip('Gas measurement', function () {
     );
   });
 
+  describe('deleverage in maintenance', function () {
+    it('for a single market', async function () {
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices([
+          indexPriceToArgumentStruct(
+            await buildIndexPriceWithValue(
+              indexPriceServiceWallet,
+              '2250.00000000',
+              baseAssetSymbol,
+            ),
+          ),
+        ]);
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .deleverageInMaintenanceAcquisition({
+          baseAssetSymbol,
+          counterpartyWallet: trader2Wallet.address,
+          liquidatingWallet: trader1Wallet.address,
+          validateInsuranceFundCannotLiquidateWalletQuoteQuantities: [
+            '22980.00000000',
+          ].map(decimalToPips),
+          liquidationBaseQuantity: decimalToPips('10.00000000'),
+          liquidationQuoteQuantity: decimalToPips('22980.00000000'),
+        });
+      console.log((await result.wait()).gasUsed.toString());
+    });
+
+    it('for 5 markets', async function () {
+      await fundWallets(
+        [trader1Wallet, trader2Wallet],
+        exchange,
+        usdc,
+        '7000.00000000',
+      );
+
+      const baseAssetSymbols = ['XYZ1', 'XYZ2', 'XYZ3', 'XYZ4'];
+      await Promise.all(
+        baseAssetSymbols.map((symbol) =>
+          addMarket(symbol, dispatcherWallet, exchange, marketStruct),
+        ),
+      );
+
+      for (const symbol of baseAssetSymbols) {
+        buyOrder.nonce = uuidv1();
+        buyOrder.market = `${symbol}-USD`;
+        buyOrderSignature = await trader2Wallet.signMessage(
+          ethers.utils.arrayify(getOrderHash(buyOrder)),
+        );
+
+        sellOrder.nonce = uuidv1();
+        sellOrder.market = `${symbol}-USD`;
+        sellOrderSignature = await trader1Wallet.signMessage(
+          ethers.utils.arrayify(getOrderHash(sellOrder)),
+        );
+
+        await exchange
+          .connect(dispatcherWallet)
+          .executeTrade(
+            ...getExecuteTradeArguments(
+              buyOrder,
+              buyOrderSignature,
+              sellOrder,
+              sellOrderSignature,
+              trade,
+            ),
+          );
+      }
+
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            [...baseAssetSymbols, baseAssetSymbol].map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPriceWithValue(
+                  indexPriceServiceWallet,
+                  '2150.00000000',
+                  symbol,
+                ),
+              ),
+            ),
+          ),
+        );
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .deleverageInMaintenanceAcquisition({
+          baseAssetSymbol,
+          counterpartyWallet: trader2Wallet.address,
+          liquidatingWallet: trader1Wallet.address,
+          validateInsuranceFundCannotLiquidateWalletQuoteQuantities: [
+            '21980.00000000',
+            '21980.00000000',
+            '21980.00000000',
+            '21980.00000000',
+            '21980.00000000',
+          ].map(decimalToPips),
+          liquidationBaseQuantity: decimalToPips('10.00000000'),
+          liquidationQuoteQuantity: decimalToPips('21980.00000000'),
+        });
+      console.log((await result.wait()).gasUsed.toString());
+    });
+  });
+
+  describe('liquidate in maintenance', function () {
+    it('for a single market', async function () {
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices([
+          indexPriceToArgumentStruct(
+            await buildIndexPriceWithValue(
+              indexPriceServiceWallet,
+              '2150.00000000',
+              baseAssetSymbol,
+            ),
+          ),
+        ]);
+      await fundWallets([insuranceFundWallet], exchange, usdc);
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .liquidateWalletInMaintenance({
+          counterpartyWallet: insuranceFundWallet.address,
+          liquidatingWallet: trader1Wallet.address,
+          liquidationQuoteQuantities: ['21980.00000000'].map(decimalToPips),
+        });
+      console.log((await result.wait()).gasUsed.toString());
+    });
+
+    it('for 5 markets', async () => {
+      await fundWallets(
+        [trader1Wallet, trader2Wallet],
+        exchange,
+        usdc,
+        '8500.00000000',
+      );
+
+      await fundWallets(
+        [insuranceFundWallet],
+        exchange,
+        usdc,
+        '10000.00000000',
+      );
+
+      const baseAssetSymbols = ['XYZ1', 'XYZ2', 'XYZ3', 'XYZ4'];
+      await Promise.all(
+        baseAssetSymbols.map((symbol) =>
+          addMarket(symbol, dispatcherWallet, exchange, marketStruct),
+        ),
+      );
+
+      for (const symbol of baseAssetSymbols) {
+        buyOrder.nonce = uuidv1();
+        buyOrder.market = `${symbol}-USD`;
+        buyOrderSignature = await trader2Wallet.signMessage(
+          ethers.utils.arrayify(getOrderHash(buyOrder)),
+        );
+
+        sellOrder.nonce = uuidv1();
+        sellOrder.market = `${symbol}-USD`;
+        sellOrderSignature = await trader1Wallet.signMessage(
+          ethers.utils.arrayify(getOrderHash(sellOrder)),
+        );
+
+        await exchange
+          .connect(dispatcherWallet)
+          .executeTrade(
+            ...getExecuteTradeArguments(
+              buyOrder,
+              buyOrderSignature,
+              sellOrder,
+              sellOrderSignature,
+              trade,
+            ),
+          );
+      }
+
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            [...baseAssetSymbols, baseAssetSymbol].map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPriceWithValue(
+                  indexPriceServiceWallet,
+                  '2150.00000000',
+                  symbol,
+                ),
+              ),
+            ),
+          ),
+        );
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .liquidateWalletInMaintenance({
+          counterpartyWallet: insuranceFundWallet.address,
+          liquidatingWallet: trader1Wallet.address,
+          liquidationQuoteQuantities: [
+            '22080.00000000',
+            '22080.00000000',
+            '22080.00000000',
+            '22080.00000000',
+            '22080.00000000',
+          ].map(decimalToPips),
+        });
+      console.log((await result.wait()).gasUsed.toString());
+    });
+  });
+
+  describe('withdraw', function () {
+    it('with no outstanding funding payments', async function () {
+      const withdrawal = {
+        signatureHashVersion,
+        nonce: uuidv1(),
+        wallet: trader1Wallet.address,
+        quantity: '1.00000000',
+        bridgeAdapter: ethers.constants.AddressZero,
+        bridgeAdapterPayload: '0x',
+      };
+      let signature = await trader1Wallet.signMessage(
+        ethers.utils.arrayify(getWithdrawalHash(withdrawal)),
+      );
+
+      await exchange
+        .connect(dispatcherWallet)
+        .withdraw(...getWithdrawArguments(withdrawal, '0.00000000', signature));
+
+      withdrawal.nonce = uuidv1();
+      signature = await trader1Wallet.signMessage(
+        ethers.utils.arrayify(getWithdrawalHash(withdrawal)),
+      );
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .withdraw(...getWithdrawArguments(withdrawal, '0.00000000', signature));
+      console.log((await result.wait()).gasUsed.toString());
+    });
+  });
+
+  describe('transfer', function () {
+    it('with no outstanding funding payments', async function () {
+      const transfer = {
+        signatureHashVersion,
+        nonce: uuidv1(),
+        sourceWallet: trader1Wallet.address,
+        destinationWallet: trader2Wallet.address,
+        quantity: '1.00000000',
+      };
+      let signature = await trader1Wallet.signMessage(
+        ethers.utils.arrayify(getTransferHash(transfer)),
+      );
+
+      await exchange
+        .connect(dispatcherWallet)
+        .transfer(...getTransferArguments(transfer, '0.00000000', signature));
+
+      transfer.nonce = uuidv1();
+      signature = await trader1Wallet.signMessage(
+        ethers.utils.arrayify(getTransferHash(transfer)),
+      );
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .transfer(...getTransferArguments(transfer, '0.00000000', signature));
+      console.log((await result.wait()).gasUsed.toString());
+    });
+  });
+
+  describe('index price publishing', async function () {
+    it('for a single market', async () => {
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices([
+          indexPriceToArgumentStruct(
+            await buildIndexPrice(indexPriceServiceWallet),
+          ),
+        ]);
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices([
+          indexPriceToArgumentStruct(
+            await buildIndexPrice(indexPriceServiceWallet),
+          ),
+        ]);
+      console.log((await result.wait()).gasUsed.toString());
+    });
+
+    it('for 5 markets', async () => {
+      const baseAssetSymbols = ['XYZ1', 'XYZ2', 'XYZ3', 'XYZ4', 'XYZ5'];
+      await Promise.all(
+        baseAssetSymbols.map((symbol) =>
+          addMarket(symbol, dispatcherWallet, exchange, marketStruct),
+        ),
+      );
+
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            baseAssetSymbols.map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPrice(indexPriceServiceWallet, symbol),
+              ),
+            ),
+          ),
+        );
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            baseAssetSymbols.map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPrice(indexPriceServiceWallet, symbol),
+              ),
+            ),
+          ),
+        );
+      console.log((await result.wait()).gasUsed.toString());
+    });
+
+    it('for 10 markets', async () => {
+      const baseAssetSymbols = [
+        'XYZ1',
+        'XYZ2',
+        'XYZ3',
+        'XYZ4',
+        'XYZ5',
+        'XYZ6',
+        'XYZ7',
+        'XYZ8',
+        'XYZ9',
+        'XYZ10',
+      ];
+      await Promise.all(
+        baseAssetSymbols.map((symbol) =>
+          addMarket(symbol, dispatcherWallet, exchange, marketStruct),
+        ),
+      );
+
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            baseAssetSymbols.map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPrice(indexPriceServiceWallet, symbol),
+              ),
+            ),
+          ),
+        );
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            baseAssetSymbols.map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPrice(indexPriceServiceWallet, symbol),
+              ),
+            ),
+          ),
+        );
+      console.log((await result.wait()).gasUsed.toString());
+    });
+
+    it('for 20 markets', async () => {
+      const baseAssetSymbols = [
+        'XYZ1',
+        'XYZ2',
+        'XYZ3',
+        'XYZ4',
+        'XYZ5',
+        'XYZ6',
+        'XYZ7',
+        'XYZ8',
+        'XYZ9',
+        'XYZ10',
+        'XYZ11',
+        'XYZ12',
+        'XYZ13',
+        'XYZ14',
+        'XYZ15',
+        'XYZ16',
+        'XYZ17',
+        'XYZ18',
+        'XYZ19',
+        'XYZ20',
+      ];
+      await Promise.all(
+        baseAssetSymbols.map((symbol) =>
+          addMarket(symbol, dispatcherWallet, exchange, marketStruct),
+        ),
+      );
+
+      await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            baseAssetSymbols.map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPrice(indexPriceServiceWallet, symbol),
+              ),
+            ),
+          ),
+        );
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .publishIndexPrices(
+          await Promise.all(
+            baseAssetSymbols.map(async (symbol) =>
+              indexPriceToArgumentStruct(
+                await buildIndexPrice(indexPriceServiceWallet, symbol),
+              ),
+            ),
+          ),
+        );
+      console.log((await result.wait()).gasUsed.toString());
+    });
+  });
+
   describe('trades', async function () {
+    it('with no outstanding funding payments and 5 open positions (limit-limit)', async () => {
+      await fundWallets(
+        [trader1Wallet, trader2Wallet],
+        exchange,
+        usdc,
+        '8500.00000000',
+      );
+
+      await fundWallets(
+        [insuranceFundWallet],
+        exchange,
+        usdc,
+        '10000.00000000',
+      );
+
+      const baseAssetSymbols = ['XYZ1', 'XYZ2', 'XYZ3', 'XYZ4'];
+      await Promise.all(
+        baseAssetSymbols.map((symbol) =>
+          addMarket(symbol, dispatcherWallet, exchange, marketStruct),
+        ),
+      );
+
+      for (const symbol of baseAssetSymbols) {
+        buyOrder.nonce = uuidv1();
+        buyOrder.market = `${symbol}-USD`;
+        buyOrderSignature = await trader2Wallet.signMessage(
+          ethers.utils.arrayify(getOrderHash(buyOrder)),
+        );
+
+        sellOrder.nonce = uuidv1();
+        sellOrder.market = `${symbol}-USD`;
+        sellOrderSignature = await trader1Wallet.signMessage(
+          ethers.utils.arrayify(getOrderHash(sellOrder)),
+        );
+
+        await exchange
+          .connect(dispatcherWallet)
+          .executeTrade(
+            ...getExecuteTradeArguments(
+              buyOrder,
+              buyOrderSignature,
+              sellOrder,
+              sellOrderSignature,
+              trade,
+            ),
+          );
+      }
+
+      buyOrder.nonce = uuidv1();
+      buyOrder.market = `${baseAssetSymbol}-USD`;
+      buyOrderSignature = await trader2Wallet.signMessage(
+        ethers.utils.arrayify(getOrderHash(buyOrder)),
+      );
+
+      sellOrder.nonce = uuidv1();
+      sellOrder.market = `${baseAssetSymbol}-USD`;
+      sellOrderSignature = await trader1Wallet.signMessage(
+        ethers.utils.arrayify(getOrderHash(sellOrder)),
+      );
+
+      const result = await exchange
+        .connect(dispatcherWallet)
+        .executeTrade(
+          ...getExecuteTradeArguments(
+            buyOrder,
+            buyOrderSignature,
+            sellOrder,
+            sellOrderSignature,
+            trade,
+          ),
+        );
+
+      console.log((await result!.wait()).gasUsed.toString());
+    });
+
     it('with no outstanding funding payments (limit-market)', async () => {
       buyOrder.type = OrderType.Market;
       buyOrder.price = '0.00000000';
@@ -190,7 +717,7 @@ describe.skip('Gas measurement', function () {
         exchange,
         dispatcherWallet,
         indexPriceServiceWallet,
-        10,
+        1,
       );
 
       const result = await exchange
@@ -230,6 +757,13 @@ describe.skip('Gas measurement', function () {
     });
 
     it('with 100 outstanding funding payments', async () => {
+      await fundWallets(
+        [trader1Wallet, trader2Wallet],
+        exchange,
+        usdc,
+        '100000.00000000',
+      );
+
       await publishFundingRates(
         exchange,
         dispatcherWallet,
@@ -252,6 +786,13 @@ describe.skip('Gas measurement', function () {
     });
 
     it('with 1000 outstanding funding payments', async () => {
+      await fundWallets(
+        [trader1Wallet, trader2Wallet],
+        exchange,
+        usdc,
+        '100000.00000000',
+      );
+
       await publishFundingRates(
         exchange,
         dispatcherWallet,
@@ -274,6 +815,13 @@ describe.skip('Gas measurement', function () {
     });
 
     it('with 6000 outstanding funding payments', async () => {
+      await fundWallets(
+        [trader1Wallet, trader2Wallet],
+        exchange,
+        usdc,
+        '100000.00000000',
+      );
+
       await publishFundingRates(
         exchange,
         dispatcherWallet,
@@ -340,4 +888,16 @@ const fundingRates = [
 ];
 function getFundingRate(index = 0): string {
   return fundingRates[index % fundingRates.length];
+}
+
+async function addMarket(
+  symbol: string,
+  dispatcherWallet: SignerWithAddress,
+  exchange: Exchange_v4,
+  marketStruct: MarketStruct,
+) {
+  const newMarketStruct = { ...marketStruct, baseAssetSymbol: symbol };
+
+  await exchange.addMarket(newMarketStruct);
+  await exchange.connect(dispatcherWallet).activateMarket(symbol);
 }
