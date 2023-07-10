b46a6456c2c064ef4783ef031c9a3a38056bbdfc
Shamsundar Dileep <dileepfrog@gmail.com>
2023-07-10 15:00:59 -0700

    chore: fix comment

--- contracts/libraries/Structs.sol
+++ contracts/libraries/Structs.sol
@@ -249,7 +249,8 @@ struct Transfer {
   uint64 grossQuantity;
   // Gas fee deducted from transfer quantity to cover dispatcher tx costs
   uint64 gasFee;
-  // The ECDSA signature of the transfer hash as produced by Hashing.getTransferHash
+  // The ECDSA signature of the transfer hash as produced by `Hashing.getTransferHash`
+dd>>>>>>> 3674bc9 (chore: fix comment)
   bytes walletSignature;
 }
 
